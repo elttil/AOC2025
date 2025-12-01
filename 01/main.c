78fc742dfc695734989ce4ef7409ab961e323638
Anton Kling <anton@kling.gg>
2025-12-01 10:19:06 +0100

    Add day01

---
+++ 01/main.c
@@ -0,0 +1,40 @@
+#include <inttypes.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+
+int main() {
+  uint64_t part1 = 0;
+  uint64_t part2 = 0;
+
+  int64_t dial = 50;
+  for (;;) {
+    char d;
+    int64_t number;
+    if (EOF == fscanf(stdin, "%c%" PRIi64 "\n", &d, &number)) {
+      break;
+    }
+    bool positive = ('R' == d);
+
+    int64_t prev = dial;
+
+    part2 += number / 100;
+    number %= 100;
+    dial += (positive) ? (number) : (-1 * number);
+
+    if ((dial < 0 && prev != 0) || dial > 100) {
+      part2++;
+    }
+    if (0 > dial) {
+      dial = 100 + dial;
+    }
+    dial %= 100;
+    if (0 == dial) {
+      part1++;
+    }
+  }
+  part2 += part1;
+  printf("part1: %" PRIu64 "\n", part1);
+  printf("part2: %" PRIu64 "\n", part2);
+  return 0;
+}
---
+++ 01/run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cc -O2 main.c -o day01
+cat input.txt | ./day01
